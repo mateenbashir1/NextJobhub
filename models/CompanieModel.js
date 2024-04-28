73db7703f062a4a9278810935ab5f616d53f38f5
Mateen Bashir <121243394+mateenbashir1@users.noreply.github.com>
2024-04-28 23:16:43 +0500

    company

--- models/CompanieModel.js
+++ models/CompanieModel.js
@@ -1,39 +1,39 @@
-// models/User.js
-
 const mongoose = require('mongoose');
 
 const CompanieSchema = new mongoose.Schema({
     name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  industry: {
-    type: String,
-    required: true
-  },
-  address:{
-    type:String,
-    required:true
-  },
-  website: {
-    type: String,
-    required: true
-  },
-  SocialMediaLinks: {
-    type: String,
-    required: true
-  },
-  UserId: {
-     type: mongoose.Schema.Types.ObjectId, ref: 'User',
- },
- logo:{
-  type:String,
-  required:true
- }
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    industry: {
+        type: String,
+        required: true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    website: {
+        type: String,
+        required: true
+    },
+    socialMediaLinks: {
+        facebook: String,
+        twitter: String,
+        instagram: String,
+        github: String,
+    },
+    UserId: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User',
+    },
+    logo:{
+        type:String,
+        required:true
+    }
 },
 { timestamps: true }
 );
