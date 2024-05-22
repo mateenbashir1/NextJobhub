08dd5895de3086e4a92f62713ec0374ec77c2bbc
Mateen Bashir <121243394+mateenbashir1@users.noreply.github.com>
2024-05-22 22:40:20 +0500

    email validation

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    // match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
   },
   password: {
     type: String,
